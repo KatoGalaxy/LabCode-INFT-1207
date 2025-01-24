a2e0f5fa03bbb3ed1c0b846b2175f02829101d66
KatoGalaxy <funguy161@hotmail.com>
2025-01-24 17:22:31 -0500

    Removed the movies list file I think

---
+++ Lab1_Group5/src/password_generator.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+def get_user_input(prompt, min_value, max_value):
+    input_loop = True
+    while input_loop == True:
+        user_input = int()
+
+def generate_password(length, num_letters, num_digits, num_specials):
+    pass   
+
+def main():
+    print("\n--- Secure Password Generator ---\n")
+    
+    password_length = 0
+    letter_count = 0
+    digit_count = 0
+    sp_char_count = 0
+    
+    setup_loop = True
+    while setup_loop == True:
+        password_length = get_user_input("Please enter the desired password length: ", 8, 128)
+
+if __name__ == "__main__":
+    main()
